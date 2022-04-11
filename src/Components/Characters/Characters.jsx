807d1b58b2f1d21e06acb01621635a7b9a07c140
Alex <alexandreliskiewicz@hotmail.com>
2022-04-11 13:01:18 +0200

    better search bar (lowercase all words) + added games and characters data

--- src/Components/Characters/Characters.jsx
+++ src/Components/Characters/Characters.jsx
@@ -1,75 +1,82 @@
-import React, {Fragment, useEffect, useState} from 'react';
+import React, { Fragment, useEffect, useState } from "react";
 // import Theoneapi from "../../Services/theoneapi";
-import CharactersInfo from './CharactersInfo';
-import Pagination from '../Pagination/Pagination';
-import MockChar from '../../mock-datas/mock-characters.json';
+import CharactersInfo from "./CharactersInfo";
+import Pagination from "../Pagination/Pagination";
+import MockChar from "../../mock-datas/mock-characters.json";
 
 const Characters = () => {
+  const [characters, setCharacters] = useState([]);
+  const [valueSubmit, setValueSubmit] = useState("");
 
-    const [characters, setCharacters] = useState([]);
-    const [valueSubmit, setValueSubmit] = useState("");
+  //for pagination
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [elementPerPage] = useState(30);
 
-    //for pagination
-    const [loading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [elementPerPage] = useState(30);
-
-    useEffect( () => {
-        setLoading(true);
-        /*
+  useEffect(() => {
+    setLoading(true);
+    /*
         Theoneapi.getAllLOTRCharacters()
         .then(data => {
             setCharacters(data.docs)
         })*/
-        setCharacters(
-            MockChar.filter((character) => character.firstname.includes(valueSubmit))
-        );
-        setLoading(false);
-    },[valueSubmit]);
-
-    if(loading) return <h2>loading...</h2>;
-
-    //pagination
-    const indexOfLastElement = currentPage * elementPerPage;
-    const indexOfFirstElement =  indexOfLastElement - elementPerPage;
-    const currentElement = characters.slice(indexOfFirstElement, indexOfLastElement);
-
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
-    const handleSearchCharacter = (e) => {
-        setValueSubmit(e.target.value);
-    }
-
-    return (
-        <Fragment>
-            <h1>My characters</h1>
-            <Pagination elementPerPage={elementPerPage} totalElement={characters.length} paginate={paginate} />
-
-            <form>
-                <input 
-                    type="text" 
-                    style={{width:"22%"}}
-                    name="searchCharacter"
-                    id="searchCharacter"
-                    onChange={handleSearchCharacter}
-                    placeholder="Search a character by his firstname"
-                />
-            </form>
-
-            <div
-            className="characterListSmartphone"
-            >
-                {currentElement.length !== 0?
-                    currentElement.map((char) => {
-                        return <CharactersInfo key={char.id} character={char} />
-                    }) : <p>No character found</p>
-
-                }
- 
-            </div>
-            
-        </Fragment>
+    setCharacters(
+      MockChar.filter((character) =>
+        character.firstname
+          .toLocaleLowerCase()
+          .includes(valueSubmit.toLocaleLowerCase())
+      )
     );
-}
+    setLoading(false);
+  }, [valueSubmit]);
+
+  if (loading) return <h2>loading...</h2>;
+
+  //pagination
+  const indexOfLastElement = currentPage * elementPerPage;
+  const indexOfFirstElement = indexOfLastElement - elementPerPage;
+  const currentElement = characters.slice(
+    indexOfFirstElement,
+    indexOfLastElement
+  );
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const handleSearchCharacter = (e) => {
+    setValueSubmit(e.target.value);
+  };
+
+  return (
+    <Fragment>
+      <h1>My characters</h1>
+      <Pagination
+        elementPerPage={elementPerPage}
+        totalElement={characters.length}
+        paginate={paginate}
+      />
+
+      <form>
+        <input
+          type="text"
+          style={{ width: "22%" }}
+          name="searchCharacter"
+          id="searchCharacter"
+          onChange={handleSearchCharacter}
+          placeholder="Search a character by his firstname"
+        />
+      </form>
+
+      <div className="characterListSmartphone">
+        {currentElement.length !== 0 ? (
+          currentElement.map((char) => {
+            return <CharactersInfo key={char.id} character={char} />;
+          })
+        ) : (
+          <p>No character found</p>
+        )}
+      </div>
+    </Fragment>
+  );
+};
 
 export default Characters;
--- src/Components/Characters/CharactersInfo.jsx
+++ src/Components/Characters/CharactersInfo.jsx
@@ -1,10 +1,13 @@
-import React , {Fragment} from 'react';
-import './charactersInfo.css';
+import React, { Fragment } from "react";
+import "./charactersInfo.css";
+import GameData from "../../mock-datas/mock-games.json";
 
 const CharactersInfo = (props) => {
-    const character = props.character;
-    
-    /*
+  const character = props.character;
+
+  const game = GameData.find((game) => game.id === character.game);
+
+  /*
             "_id": "5cd99d4bde30eff6ebccfbbe",
             "height": "",
             "race": "Human",
@@ -17,18 +20,21 @@ const CharactersInfo = (props) => {
             "name": "Adanel",
             "wikiUrl": "http://lotr.wikia.com//wiki/Adanel"
     */
-    return (
-        <Fragment>
-            <div className='charactersInfoCard'>
-                <h2>{character.firstname + " " + character.lastname} </h2>
-                <h4>{character.job}</h4>
-                <p>{character.race}</p>
-                <p>{character.gender}</p>
-                <p>{character.age} year </p>
-                <p>{character.from}</p>
-            </div>    
-        </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      <div className="charactersInfoCard">
+        <h2>{character.firstname + " " + character.lastname} </h2>
+        <h4>{character.job}</h4>
+        <p>
+          Played by : {character.player} in game {game.name}
+        </p>
+        <p>{character.race}</p>
+        <p>{character.gender}</p>
+        <p>{character.age} year </p>
+        <p>{character.from}</p>
+      </div>
+    </Fragment>
+  );
+};
 
 export default CharactersInfo;
